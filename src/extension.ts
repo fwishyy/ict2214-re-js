87bccb82a2ac5ac3bb7f039dd633dc6e628f7cc2
fwishyy <fwishytf2@gmail.com>
2025-02-20 05:13:10 +0800

    Add ExpressionSimplifier to undo obfuscated constants

--- src/extension.ts
+++ src/extension.ts
@@ -14,6 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 		unpackArrays: true,
 		decodeStrings: true,
 		removeProxyFunctions: true,
+		simplifyExpressions: true
 	});
 
 	const myScheme = 'deobfuscate';
