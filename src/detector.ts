2a38ceefb2234c23ea9024cce50f027c720ee43c
fwishyy <fwishytf2@gmail.com>
2025-03-06 10:26:23 +0800

    WIP detection

--- src/detector.ts
+++ src/detector.ts
@@ -3,6 +3,8 @@ import { parse } from '@babel/parser';
 import traverse from '@babel/traverse';
 import vscode from 'vscode';
 
+import Config from './config';
+
 export default class Detector {
     private code: string;
     private ast: types.File;
@@ -11,11 +13,52 @@ export default class Detector {
         this.ast = parse(code);
         this.code = code;
     }
-    execute() {
-        if (detectProxyFunction(this.ast)) {
-            vscode.window.showInformationMessage('Proxy Function Detected');
+
+    // TODO: Add more detection techniques
+    execute(): Config | null {
+        // detect common obfusctors first
+
+
+        const generatedConfig: Config = new Config({
+            unpackArrays: false,
+            decodeStrings: false,
+            removeProxyFunctions: false,
+            simplifyExpressions: false,
+            removeDeadCode: false,
+            stringProxyFunctions: false
+        });
+
+        if (detectJSFuck(this.code)) {
+            vscode.window.showErrorMessage("It looks like the code is obfuscated with JSFuck, please use a suitable decoder");
+            return generatedConfig;
         }
+
+        if (detectProxyFunction(this.ast)) {
+            vscode.window.showInformationMessage("Proxy functions detected");
+            generatedConfig.removeProxyFunctions = true;
+        }
+
+        if (detectEncodedString(this.ast)) {
+            vscode.window.showInformationMessage("Encoded strings detected");
+            generatedConfig.decodeStrings = true;
+        }
+
+        // display notification if no classical techniques are detected
+        if (!Object.values(generatedConfig).some((value) => value === true)) {
+            vscode.window.showInformationMessage('No classical techniques detected, check if the file is obfuscated');
+        }
+
+        return generatedConfig;
     }
+}
+
+function detectJSFuck(code: string): boolean {
+    // check that code contains only []()!+
+    if (code.length === 0) {
+        return false;
+    };
+    const regex = /[^[\]()!+]/g;
+    return !regex.test(code);
 }
 
 function detectProxyFunction(ast: types.File): boolean {
@@ -26,6 +69,23 @@ function detectProxyFunction(ast: types.File): boolean {
             if (body.length === 1 &&
                 types.isReturnStatement(body[0]) &&
                 types.isMemberExpression(body[0].argument)) {
+                test = true;
+                return;
+            }
+        }
+    });
+
+    return test;
+}
+
+// TODO: Maybe change this detection, it's a little scuffed
+function detectEncodedString(ast: types.File): boolean {
+    let test = false;
+    traverse(ast, {
+        StringLiteral(path: any) {
+            let { raw, rawValue } = path.node.extra;
+            raw = raw.replaceAll('"', '');
+            if (rawValue !== raw) {
                 test = true;
                 return;
             }
--- src/extension.ts
+++ src/extension.ts
@@ -10,112 +10,126 @@ import Detector from './detector';
 // This is the function that is called when the extension is activated
 // Currently configured to a super barebones preview of the current file
 export function activate(context: vscode.ExtensionContext) {
-	const config = new Config({
-		unpackArrays: true,
-		decodeStrings: true,
-		removeProxyFunctions: true,
-		simplifyExpressions: true,
-		removeDeadCode: true,
-		stringProxyFunctions: true
-	});
+    const config = new Config({
+        unpackArrays: true,
+        decodeStrings: true,
+        removeProxyFunctions: true,
+        simplifyExpressions: true,
+        removeDeadCode: true,
+        stringProxyFunctions: true
+    });
 
-	const myScheme = 'deobfuscate';
-	const myProvider = new class implements vscode.TextDocumentContentProvider {
-		onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
-		onDidChange = this.onDidChangeEmitter.event;
+    let previewPanel: vscode.WebviewPanel;
 
-		provideTextDocumentContent(uri: vscode.Uri): string {
-			return uri.query;
-		}
-	};
-	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider));
+    const myScheme = 'deobfuscate';
+    const myProvider = new class implements vscode.TextDocumentContentProvider {
+        onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+        onDidChange = this.onDidChangeEmitter.event;
 
-	context.subscriptions.push(vscode.commands.registerCommand('deobfuscate.detect', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
-		const code = editor.document.getText();
-		const detector = new Detector(code);
-		detector.execute();
-	}));
+        provideTextDocumentContent(uri: vscode.Uri): string {
+            return uri.query;
+        }
+    };
+    context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider));
 
-	// register command to preview deobfuscated code
-	context.subscriptions.push(vscode.commands.registerCommand('deobfuscate.preview', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return;
-		}
-		const src = editor.document.getText();
+    context.subscriptions.push(vscode.commands.registerCommand('rejs.detect', async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        const code = editor.document.getText();
+        const detector = new Detector(code);
+        const generatedConfig = detector.execute();
 
-		const deobfuscator = new Deobfuscator(config, src);
-		const deobfuscatedSrc = deobfuscator.execute();
+        if (generatedConfig && previewPanel) {
+            // send message to webview to update checkbox
+            previewPanel.webview.postMessage({
+                type: 'updateCheckbox',
+                options: generatedConfig
+            });
+        }
+    }));
 
-		const fileName = editor.document.fileName + '.deobfuscated';
-		const uri = vscode.Uri.parse(`${myScheme}://deobfuscate-preview/${fileName}?${deobfuscatedSrc}`);
-		const doc = await vscode.workspace.openTextDocument(uri);
-		await vscode.window.showTextDocument(doc, vscode.ViewColumn.Two);
-	}));
+    // register command to preview deobfuscated code
+    context.subscriptions.push(vscode.commands.registerCommand('deobfuscate.preview', async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        const src = editor.document.getText();
 
-	let lastActiveEditor: any = null; // Store the last focused editor
+        const deobfuscator = new Deobfuscator(config, src);
+        const deobfuscatedSrc = deobfuscator.execute();
 
-	vscode.window.onDidChangeActiveTextEditor((editor) => {
-		if (editor) {
-			lastActiveEditor = editor;
-		}
-	});
+        const fileName = editor.document.fileName + '.deobfuscated';
+        const uri = vscode.Uri.parse(`${myScheme}://deobfuscate-preview/${fileName}?${deobfuscatedSrc}`);
+        const doc = await vscode.workspace.openTextDocument(uri);
+        await vscode.window.showTextDocument(doc, vscode.ViewColumn.Two);
+    }));
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('rejs.activate', () => {
-			// once it's activated, lock in the first active editor
+    context.subscriptions.push(
+        vscode.commands.registerCommand('rejs.activate', () => {
+            // once it's activated, lock in the first active editor
 
-			const editor = vscode.window.activeTextEditor;
-			const panel = vscode.window.createWebviewPanel(
-				'deobfuscate',
-				'Deobfuscator',
-				vscode.ViewColumn.Two,
+            const editor = vscode.window.activeTextEditor;
+            // if panel is null, create a new one
+            if (!previewPanel) {
+                previewPanel = vscode.window.createWebviewPanel(
+                    'deobfuscate',
+                    'Deobfuscator',
+                    vscode.ViewColumn.Two,
+                    {
+                        enableScripts: true
+                    }
+                );
 
-				{
-					enableScripts: true
-				}
-			);
+                previewPanel.webview.html = getWebviewContent();
 
-			panel.webview.html = getWebviewContent();
+                // Handle messages from the webview
+                previewPanel.webview.onDidReceiveMessage(
+                    async (options) => {
+                        const config = new Config(options);
+                        if (!editor) {
+                            return;
+                        }
+                        const src = editor.document.getText();
 
-			// Handle messages from the webview
-			panel.webview.onDidReceiveMessage(
-				async (options) => {
-					const config = new Config(options);
-					if (!editor) {
-						return;
-					}
-					const src = editor.document.getText();
+                        const deobfuscator = new Deobfuscator(config, src);
+                        const deobfuscatedSrc = deobfuscator.execute();
 
-					const deobfuscator = new Deobfuscator(config, src);
-					const deobfuscatedSrc = deobfuscator.execute();
+                        const fileName = editor.document.fileName + '.deobfuscated';
+                        const uri = vscode.Uri.parse(`${myScheme}://deobfuscate-preview/${fileName}?${deobfuscatedSrc}`);
+                        const doc = await vscode.workspace.openTextDocument(uri);
+                        await vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
+                    },
+                    undefined,
+                    context.subscriptions
+                );
+            } else {
+                previewPanel.reveal(vscode.ViewColumn.Two);
+            }
+        })
+    );
 
-					const fileName = editor.document.fileName + '.deobfuscated';
-					const uri = vscode.Uri.parse(`${myScheme}://deobfuscate-preview/${fileName}?${deobfuscatedSrc}`);
-					const doc = await vscode.workspace.openTextDocument(uri);
-					await vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
-				},
-				undefined,
-				context.subscriptions
-			);
-		})
-	);
-
-	const rejsButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    const rejsButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
     rejsButton.command = 'rejs.activate';
     rejsButton.text = `$(globe) Activate RE:JS`;
     rejsButton.tooltip = 'Click to activate RE:JS';
     rejsButton.show();
 
     context.subscriptions.push(rejsButton);
+
+    const detectButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    detectButton.command = 'rejs.detect';
+    detectButton.text = `$(search) RE:JS Detect`;
+    detectButton.tooltip = 'Click to run detection engine';
+    detectButton.show();
+
+    context.subscriptions.push(detectButton);
 }
 
 function getWebviewContent() {
-	return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -223,6 +237,18 @@ function getWebviewContent() {
                     };
 
                     vscode.postMessage(options);
+                });
+
+                window.addEventListener('message', event => {
+                    const message = event.data; // The JSON data our extension sent
+                    // update checkboxes
+                    document.getElementById('unpackArrays').checked = message.options.unpackArrays;
+                    document.getElementById('decodeStrings').checked = message.options.decodeStrings;
+                    document.getElementById('removeProxyFunctions').checked = message.options.removeProxyFunctions;
+                    document.getElementById('simplifyExpressions').checked = message.options.simplifyExpressions;
+                    document.getElementById('removeDeadCode').checked = message.options.removeDeadCode;
+                    document.getElementById('stringProxyFunctions').checked = message.options.stringProxyFunctions;
+                    
                 });
             }())
         </script>
